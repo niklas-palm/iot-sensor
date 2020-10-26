96750dafc5c044740f5955629f56d1ce21fba14f
Palm <nipalm@3c22fb386d48.ant.amazon.com>
2020-10-26 10:31:20 +0100

    Add AppSync placeholder to template file

--- frontend/src/aws-exports-template.js
+++ frontend/src/aws-exports-template.js
@@ -9,6 +9,9 @@ const awsmobile = {
   aws_cognito_region: "",
   aws_user_pools_id: "",
   aws_user_pools_web_client_id: "",
+  aws_appsync_graphqlEndpoint: "",
+  aws_appsync_region: "",
+  aws_appsync_authenticationType: "AWS_IAM",
 };
 
 export default awsmobile;
