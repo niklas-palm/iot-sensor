22f979db4400d1cb1277d6a17d1e468b54f20eb2
Palm <nipalm@3c22fb386d48.ant.amazon.com>
2020-10-26 10:32:51 +0100

    Update frontend to fetch all sensors and subscribe to changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Amplify from "aws-amplify";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import logo from "./logo.svg";
 import "./App.scss";
@@ -7,9 +7,45 @@ import "@aws-amplify/ui/dist/style.css"; // Styles for login component
 
 import { withAuthenticator } from "aws-amplify-react";
 
+import { allSensors } from "./graphql/queries";
+import { onAddSensor } from "./graphql/subscriptions";
+
 Amplify.configure(awsconfig);
 
 function App() {
+  const [sensors, setSensors] = React.useState({});
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const res = await API.graphql(graphqlOperation(allSensors));
+      let init_sensors = {};
+      res.data.allSensors.sensors.forEach((sensor) => {
+        init_sensors[sensor.SensorId] = sensor;
+      });
+      setSensors(init_sensors);
+      return;
+    };
+
+    const subToChanges = () => {
+      const subscription = API.graphql(graphqlOperation(onAddSensor)).subscribe(
+        {
+          next: (payload) => {
+            // console.log(payload.value.data.onAddSensor);
+            let newSensor = payload.value.data.onAddSensor;
+            // Do something with the data
+            setSensors((prev) => ({
+              ...prev,
+              [newSensor.SensorId]: newSensor,
+            }));
+          },
+        }
+      );
+    };
+
+    getData();
+    subToChanges();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
